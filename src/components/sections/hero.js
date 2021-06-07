17fb654686c45275ed27b0ee4219350d95ad7541
Anir0y-MacPro <animeshroy@live.com>
2021-06-07 17:44:35 +0530

    header

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,7 +60,7 @@ const Hero = () => {
 
   const one = <h1>Hi, my name is</h1>;
   const two = <h2 className="big-heading">Animesh Roy.</h2>;
-  const three = <h3 className="big-heading">I break security for living!</h3>;
+  const three = <h3 className="big-heading">I break security for a living!</h3>;
   const four = (
     <p>
       I'm a Hyderabad-based CyberSecurity Consultant, The CEO of ZeroBit solutions LLP, 
